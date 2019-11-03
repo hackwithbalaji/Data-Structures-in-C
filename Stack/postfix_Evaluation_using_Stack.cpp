ce227eacef7c82cb0091e8cccbba5d2cd761e78b
Balaji <hackwithbalaji@gmail.com>
2019-11-03 21:28:14 +0530

    Add files via upload

---
+++ Stack/postfix_Evaluation_using_Stack.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stack>
+#include<string.h>
+using namespace std;
+int operation(int opt,int x,int y)
+{
+    int res;
+    if(opt=='+')
+    {
+        res=x+y;
+    }
+    else if(opt=='-')
+    {
+        res=x-y;
+    }
+    else if(opt=='*')
+    {
+        res=x*y;
+    }
+    else if(opt=='/')
+    {
+        res=x/y;
+    }
+    return res;
+}
+int main()
+{
+      char arr[100];
+      int i;
+      stack<int> s;
+      cout<<"Enter Postfix Expression to Evaluate : ";
+      cin.get(arr,100);
+      for(i=0;i<strlen(arr);i++)
+      {
+          if(arr[i]!='+' && arr[i]!='-' && arr[i]!='*' && arr[i]!='/')
+          {
+              s.push(arr[i]-'0');
+          }
+          else if(arr[i]=='+' || arr[i]=='-' || arr[i]=='*' || arr[i]=='/')
+          {
+              int y=s.top();
+              s.pop();
+              int x=s.top();
+              s.pop();
+              int res=operation(arr[i],x,y);
+              s.push(res);
+          }
+      }
+      cout<<s.top();
+      return 0;
+}
